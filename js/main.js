5b4add372763a16a0f07278d2f450115ac4b340d
Michal Korybski <michalkorybski@michalkskismini.home>
2019-01-11 00:06:51 +0000

    Change: tabindex and listbox roled added in listings page

--- js/main.js
+++ js/main.js
@@ -157,8 +157,10 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
+  li.setAttribute('tabindex', '0');
 
   const image = document.createElement('img');
+  image.setAttribute('tabindex', '0');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
